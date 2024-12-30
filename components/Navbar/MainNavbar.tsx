3a4b6cc85e7a3d11af63365e53b3bcdaef0cb25d
MJ-Hesari <javadhe3ari@gmail.com>
2024-12-30 18:02:12 +0330

    refactor: update navbar ui

--- components/Navbar/MainNavbar.tsx
+++ components/Navbar/MainNavbar.tsx
@@ -11,34 +11,39 @@ import SearchInput from "./SearchInput";
 import { Icon } from "@iconify/react";
 import { DictsTypes } from "@/app/[lang]/dictionaries/dictionaries";
 import Image from "next/image";
-import img1 from '@/public/drziba.jpg'
+import img1 from "@/public/drziba.jpg";
 export default function MainNavbar({ dicts }: { dicts: DictsTypes }) {
   return (
     <Navbar
       maxWidth="full"
-      classNames={{ wrapper: "!container mx-auto px-0", base: "bg-white" }}
+      classNames={{
+        wrapper:
+          "!container mx-auto px-0 !grid grid-cols-[300px_auto_max-content] justify-normal px-4",
+        base: "bg-white",
+      }}
       className="border-b hidden lg:block shadow-sm"
     >
-      <NavbarBrand className=" max-w-60">
+      <NavbarBrand>
         <Link href="/" className="flex items-center gap-3 text-black">
-
           {process?.env?.NEXT_PUBLIC_DIRECTORY === "drziba" ? (
             <>
-            <Image src={img1} alt="logo" width={45} className="rounded-full"/>
-            <h1>{dicts.DirectoryName.drziba}</h1>
+              <Image
+                src={img1}
+                alt="logo"
+                width={45}
+                className="rounded-full"
+              />
+              <h1>{dicts.DirectoryName.drziba}</h1>
             </>
-            ) : (
-              <>
+          ) : (
+            <>
               <Logo />
               <h1>{dicts.DirectoryName.exchange}</h1>
-              </>
+            </>
           )}
         </Link>
       </NavbarBrand>
-      <NavbarContent
-        className="flex-1 flex justify-center items-center"
-        justify="start"
-      >
+      <NavbarContent>
         {/* <NavbarItem>
           <Dropdown>
             <DropdownTrigger>
@@ -66,21 +71,23 @@ export default function MainNavbar({ dicts }: { dicts: DictsTypes }) {
           </Link>
         </NavbarItem>
       </NavbarContent> */}
-      <NavbarContent justify="end" className="gap-4 flex-1">
-        {!process.env.IS_NOT_MULTILANG ?<NavbarItem>
-          <Button
-            as="a"
-            variant="bordered"
-            size="sm"
-            className="text-default-500 rounded-xl hover:text-secondary-600 hover:border-secondary-600"
-            href="/languages"
-            aria-label="language page"
-            startContent={<Icon icon="mdi:translate" className="h-4 w-4" />}
-          >
-            Languages
-          </Button>
-        </NavbarItem>: null}
-        
+      <NavbarContent className="gap-4">
+        {!process.env.IS_NOT_MULTILANG ? (
+          <NavbarItem>
+            <Button
+              as="a"
+              variant="bordered"
+              size="sm"
+              className="text-default-500 rounded-xl hover:text-secondary-600 hover:border-secondary-600"
+              href="/languages"
+              aria-label="language page"
+              startContent={<Icon icon="mdi:translate" className="h-4 w-4" />}
+            >
+              Languages
+            </Button>
+          </NavbarItem>
+        ) : null}
+
         {/* <NavbarItem>
           <UserAvatar />
         </NavbarItem>
